d397fc628c670c8e2b3bd35c1c78775fb5818bea
hirurana <zcabhra@ucl.ac.uk>
2018-03-19 15:50:46 +0000

    Can I suck ur nan's toe pls?

--- Hiru's House/final.c
+++ Hiru's House/final.c
@@ -7,30 +7,7 @@
 #include "simpletools.h"
 #include "ping.h"
 #include "simulator.h"
-
-//creating map within program
-//if wall present then val will be 1 else 0
-char facing = 'n';
-struct mazeMap {
-  int north;
-  int east;
-  int south;
-  int west;
-};
-
-struct mazeMap cells[4][4];
-
-//can't init any other way
-void init(){
-	for(int i = 0; i < 4; i++){
-		for(int j = 0; j< 4; j++){
-			cells[i][j].north = 0;
-			cells[i][j].east = 0;
-			cells[i][j].south = 0;
-			cells[i][j].west = 0;
-		}
-	}
-}
+#include "init.h"
 
 //setting up map maker
 void makeMap(char direction, int x,int y ,int left, int right, int front){
