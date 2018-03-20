373af57782e5c5f1cac39270c2cddd283e71c1eb
hirurana <zcabhra@ucl.ac.uk>
2018-03-20 20:02:00 +0000

    if you werent a shit coder, we;d get this done faster

--- final.c
+++ final.c
@@ -10,6 +10,10 @@
 #include "init.h"
 #include "sensors.h"
 
+int route[60][2];
+int routeCount = 0;
+int listRec = 1; //use to record all cells visited before visiting 3,3
+
 //setting up map maker
 void makeMap(char direction, int x,int y ,int left, int right, int front){
   if (left < 10) {
@@ -27,6 +31,7 @@ void makeMap(char direction, int x,int y ,int left, int right, int front){
 void changeNewDirection(currentFacing, numOfTurns){
   facing = (numOfTurns + currentFacing) % 4;
   if (facing < 0) {facing += 4;}
+  printf("%d\n", facing);
 }
 
 int main (){
@@ -35,11 +40,11 @@ int main (){
   int x = 0;
   int y = -1;
   int i = 1;
+  int turns;
   drive_goto(33,33);
   while (i){
-    // if at cells (3,0) get currenting facing, find the diif current and 3 change facing to 3
     drive_goto(square_dist, square_dist);
-    int turns;
+    printf("turns %d\n", turns);
     changeNewDirection(facing, turns);
 
     switch (facing) {
@@ -60,80 +65,74 @@ int main (){
     int IRRight = dist("RIGHT");
     int front = ping_cm(8);
 
+
+    if (x == 3 && y == 3) {
+      listRec = 0;
+    }
+    // printf("%d\n", listRec);
+    // printf("%d\n", facing);
+
     makeMap(facing,x,y,IRLeft, IRRight, front);
-    //180
+
+    // add to array
+    if (listRec == 1) {
+      route[routeCount][0] = x;
+      route[routeCount][1] = y;
+      for (int i = 0; i < routeCount; i++) {
+        for (int j = 0; j < 2; j++) {
+          printf("%d ", route[i][j]);
+        }
+        printf("\n");
+      }
+      routeCount++;
+    }
+
+    //180 turn
     if(isWallSide(IRLeft) && isWallSide(IRRight) && isWallFront(front)){
-      //drive_goto(50,-49);
-      //facing = 's';
-      printf("Left %i \n", IRLeft);
-      printf("Right %i \n", IRRight);
-      printf("Front %i \n", front);
       drive_speed(25,-25);
       pause(2044);
       drive_speed(0,0);
       pause(100);
       turns = 2;
-      continue;
-    }
- 
-    //Turn left
-    if(isWallSide(IRRight) && !isWallFront(front) && !isWallSide(IRLeft)){
-      //drive_goto(-24,25);
-      printf("Left %i \n", IRLeft);
-      printf("Right %i \n", IRRight);
-      printf("Front %i \n", front);
+    }//Turn left if blah blah (fucking write it you bitch)
+    else if(isWallSide(IRRight) && !isWallFront(front) && !isWallSide(IRLeft)){
       drive_speed(-25, 25);
       pause(1022);
       drive_speed(0,0);
       pause(100);
       turns = -1;
-      continue;
-    }
-
-
+    } else
     //Turn left
     if(isWallSide(IRRight) && isWallFront(front) && !isWallSide(IRLeft)){
-      //drive_goto(-24,25);
-      printf("Left %i \n", IRLeft);
-      printf("Right %i \n", IRRight);
-      printf("Front %i \n", front);
       drive_speed(-25, 25);
       pause(1022);
       drive_speed(0,0);
       pause(100);
       turns = -1;
-      continue;
-    }
+    } else
     //Turn left
     if(isWallFront(front) && !isWallSide(IRLeft) && !isWallSide(IRRight)){
-      //drive_goto(-24,25);
-      printf("Left %i \n", IRLeft);
-      printf("Right %i \n", IRRight);
-      printf("Front %i \n", front);
       drive_speed(-25, 25);
       pause(1022);
       drive_speed(0,0);
       pause(100);
       turns = -1;
-      continue;
-    }
-
+    } else
    //Turn Right
     if(isWallSide(IRLeft) && isWallFront(front) && !isWallSide(IRRight)){
-      //drive_goto(25,-24);
-      printf("Left %i \n", IRLeft);
-      printf("Right %i \n", IRRight);
-      printf("Front %i \n", front);
       drive_speed(25, -25);
       pause(1022);
       drive_speed(0,0);
       pause(100);
      turns = 1;
-      continue;
+    } else {
+      turns = 0;
     }
 
+
+
     // printf("front %d\n", front);
-    
+
     // printf("Left %i \n" , IRLeft);
     // printf("Right %i \n", IRRight);
     i++;
